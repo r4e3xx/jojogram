a91578132b9cbc18d868b7df6427860654705da7
r4e3xx <166480972+r4e3xx@users.noreply.github.com>
2024-05-17 13:58:14 +0500

    Add files via upload

---
+++ jslol.js
@@ -0,0 +1,66 @@
+
+            let lik1=document.getElementById('like1');
+            let dis1=document.getElementById('dis1');
+            function like1(){
+                if(document.getElementById('cnt1')=='0'){
+                    document.getElementById('cnt1')='1';
+                    lik1.style.opacity="100%";
+                    dis1.style.opacity=0;
+                }else{
+                    document.getElementById('cnt1')='0';
+                    lik1.style.opacity=0;
+                    dis1.style.opacity="100%";
+                }
+            }
+            let lik2=document.getElementById('like2');
+            let dis2=document.getElementById('dis2');
+            function like2(){
+                if(document.getElementById('cnt2')=='0'){
+                    document.getElementById('cnt2')='1';
+                    lik2.style.opacity="100%";
+                    dis2.style.opacity=0;
+                }else{
+                    document.getElementById('cnt2')='0';
+                    lik2.style.opacity=0;
+                    dis2.style.opacity="100%";
+                }
+            }
+            let lik3=document.getElementById('like3');
+            let dis3=document.getElementById('dis3');
+            function like3(){
+                if(document.getElementById('cnt3')=='0'){
+                    document.getElementById('cnt3')='1';
+                    lik3.style.opacity="100%";
+                    dis3.style.opacity=0;
+                }else{
+                    document.getElementById('cnt3')='0';
+                    lik3.style.opacity=0;
+                    dis3.style.opacity="100%";
+                }
+            }
+            let lik4=document.getElementById('like4');
+            let dis4=document.getElementById('dis4');
+            function like4(){
+                if(document.getElementById('cnt4')=='0'){
+                    document.getElementById('cnt4')='1';
+                    lik4.style.opacity="100%";
+                    dis4.style.opacity=0;
+                }else{
+                    document.getElementById('cnt4')='0';
+                    lik4.style.opacity=0;
+                    dis4.style.opacity="100%";
+                }
+            }
+            let lik5=document.getElementById('like5');
+            let dis5=document.getElementById('dis5');
+            function like5(){
+                if(document.getElementById('cnt5')=='0'){
+                    document.getElementById('cnt5')='1';
+                    lik5.style.opacity="100%";
+                    dis5.style.opacity=0;
+                }else{
+                    document.getElementById('cnt5')='0';
+                    lik5.style.opacity=0;
+                    dis5.style.opacity="100%";
+                }
+            }
